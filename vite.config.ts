d5777cf24b4baa9be221af4605a9699fd25b0889
ntsondct2021 <ntsondct2021@gmail.com>
2026-01-16 10:13:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Đảm bảo các file build sử dụng đường dẫn tương đối
+  base: '/noel/', // Đảm bảo các file build sử dụng đường dẫn tương đối
   build: {
     outDir: 'dist',
     emptyOutDir: true,
